7baababc11fa5d0ebe97bc1a3bda4e2911916a80
sivasthigan1999 <ssivasthigan@gmail.com>
2022-01-30 06:35:12 +0530

    Create colourdetection.py

---
+++ colourdetection.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
+
+def empty(a):
+    pass
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+while True:
+
+    _, img = cap.read()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #cv2.imshow("original", img)
+    #cv2.imshow("Result", imgHSV)
+
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+    cv2.imshow("original", img)
+    cv2.imshow("mask", mask)
+    cv2.imshow("imgResult", imgResult)
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+
+        break
